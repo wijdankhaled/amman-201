c13a17db46e8ff983ea92fd7095fc425862f2eaf
wijdankhaled <wijdankhaled178@gmail.com>
2021-04-25 13:41:53 +0300

    201 repo

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 let uName = prompt('please enter your name');
 alert('nice name');
-document.write(uName);
+console.log(uName);
+//document.write(uName);
+
 
 let favFood = prompt('what is your favert food');
-document.write(favFood,<p>'It's a delicious meal, 
-and it's important to eat what we love, certainly taking into account what's good or not for health.'</p>);
+console.log(favFood);
+//document.write(favFood,<p>'It's a delicious meal, 
+//and it's important to eat what we love, certainly taking into account what's good or not for health.'</p>);
+//alert("please choose frome these meal: pizza soup spaghetti ");
+
+let color = prompt('what is your fav color');
+console.log(color);
+
+let age =prompt('please enter your age');
+console.log(age);
+
+alert('hello'+uName+'your favert food is'+favFood+'your fav color is'+color+'and finally ypur age is'+age);
+
