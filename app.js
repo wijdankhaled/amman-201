aabdf1739dd463f9dc6ee308c8b987e6cbd971ef
wijdankhaled <wijdankhaled178@gmail.com>
2021-04-25 13:37:25 +0300

    201repo

---
+++ app.js
@@ -0,0 +1,7 @@
+let uName = prompt('please enter your name');
+alert('nice name');
+document.write(uName);
+
+let favFood = prompt('what is your favert food');
+document.write(favFood,<p>'It's a delicious meal, 
+and it's important to eat what we love, certainly taking into account what's good or not for health.'</p>);
